beb8bdb15d893cfbb89b856c133e57d0d14dd43f
Rudy Gevaert <rudy@webworm.org>
2012-01-19 22:15:49 +0100

    Basis apache config.

--- manifests/webinterface/apache.pp
+++ manifests/webinterface/apache.pp
@@ -5,5 +5,15 @@ class graylog2::webinterface::apache
       ensure => installed;
     'libapache2-mod-passenger':
       ensure => installed;
+    'bundler':
+      ensure => installed,
+      provider => gem;
+  }
+  
+  file {
+    '/etc/apache2/sites-enabled/000-default':
+      ensure  => present,
+      require => Package['libapache2-mod-passenger'],
+      content => template('graylog2/vhost.erb');
   }
 }
